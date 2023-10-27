3762528be9040f139a6d4f0bacf59ca37e2cff1c
Anurag Kumar <anurag.hcu@gmail.com>
2023-10-27 11:13:02 +0530

    grid traveler

--- dynamicprogramming/gridtraveller_test.go
+++ dynamicprogramming/gridtraveller_test.go
@@ -1,11 +1,23 @@
 package dynamicprogramming
 
-import
-(
-	"testing"
+import (
 	"fmt"
+	"testing"
 )
+
 func TestGridTraveller(t *testing.T) {
 	fmt.Println("tests")
-	
+
+}
+
+/*
+
+Traveller on a 2d grid,start at top left corner and goal is to reach bottom right corner
+can move either right or down.
+How many ways you can travel on a grid from the top left to the bottom right for dimension m*n
+
+*/
+
+func gridTraveller(n int, m int) int {
+	return 0
 }
