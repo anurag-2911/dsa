b4fb8c864184a3994b67552f930e3aea7ce62404
Anurag Kumar <anurag.hcu@gmail.com>
2023-11-18 09:13:53 +0530

    recursion/recursion_test.go

--- recursion/recursion_test.go
+++ recursion/recursion_test.go
@@ -2,6 +2,7 @@ package recursion
 
 import (
 	"fmt"
+	"strings"
 	"testing"
 )
 
@@ -15,10 +16,42 @@ func revstring(s string) string {
 	if len(s) == 0 {
 		return s
 	}
-	substr:=s[1:]
-	first:=s[0]
-	strfirst:=string(first)
+	substr := s[1:]
+	first := s[0]
+	strfirst := string(first)
 	fmt.Print(strfirst)
 	fmt.Print(" ")
 	return revstring(substr) + string(first)
+}
+
+func TestPalinDrome(t *testing.T){
+	fmt.Println(isPalindrome("racecar"))
+}
+
+func isPalindrome(s string) bool {
+	s = strings.ReplaceAll(s, " ", "")
+	s = strings.ToLower(s)
+	return checkPalindrome(s, 0, len(s)-1)
+
+}
+func checkPalindrome(s string, start, end int) bool {
+	if(start>=end){
+		return true
+	}
+	if(s[start]!=s[end]){
+		return false
+	}
+	return checkPalindrome(s,start+1,end-1)
+}
+func checkPalin(s string)bool{
+	if(len(s)==0 || len(s)==1){
+		return true
+	}
+	if(s[0]==s[len(s)-1]){
+		return checkPalin(s[1:len(s)-1])
+	}
+	return false
+}
+func TestCheckPalin(t *testing.T){
+	fmt.Println(checkPalin("racecar"))
 }
