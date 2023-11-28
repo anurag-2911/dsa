4aeed63edb0326d468ea71bfd2b080f87f183f3d
Anurag Kumar <anurag.hcu@gmail.com>
2023-11-29 04:58:40 +0530

    recursion basics.

--- recursion/rec_test.go
+++ recursion/rec_test.go
@@ -1,10 +1,47 @@
 package recursion
 
-import
-(
+import (
+	"fmt"
 	"testing"
 )
 
 func TestZXxx(t *testing.T) {
-	
+
+}
+
+func xrevstring(s string) string {
+	if len(s) == 0 {
+		return ""
+	}
+	substr := s[1:]
+	first := string(s[0])
+	return xrevstring(substr) + first
+
+}
+func TestRevString(t *testing.T) {
+	fmt.Println(xrevstring("hallo"))
+}
+
+func xpalin(s string) bool {
+
+	if(len(s)==0 || len(s)==1){
+		return true
+	}
+	firstChar := s[0]
+	lastchar := s[len(s)-1]
+	if firstChar == lastchar {
+		substr := s[1 : len(s)-1]
+		return xpalin(substr)
+		
+	}
+
+	return false
+}
+func TestXPlain(t *testing.T){
+	fmt.Println(xpalin("racecar"))
+	fmt.Println(xpalin("racegurram"))
+}
+
+func xdectobin(s string)string{
+	return ""
 }
--- recursion/recursion_test.go
+++ recursion/recursion_test.go
@@ -19,13 +19,11 @@ func revstring(s string) string {
 	}
 	substr := s[1:]
 	first := s[0]
-	strfirst := string(first)
-	fmt.Print(strfirst)
-	fmt.Print(" ")
+
 	return revstring(substr) + string(first)
 }
 
-func TestPalinDrome(t *testing.T){
+func TestPalinDrome(t *testing.T) {
 	fmt.Println(isPalindrome("racecar"))
 }
 
@@ -36,33 +34,33 @@ func isPalindrome(s string) bool {
 
 }
 func checkPalindrome(s string, start, end int) bool {
-	if(start>=end){
+	if start >= end {
 		return true
 	}
-	if(s[start]!=s[end]){
+	if s[start] != s[end] {
 		return false
 	}
-	return checkPalindrome(s,start+1,end-1)
+	return checkPalindrome(s, start+1, end-1)
 }
-func checkPalin(s string)bool{
-	if(len(s)==0 || len(s)==1){
+func checkPalin(s string) bool {
+	if len(s) == 0 || len(s) == 1 {
 		return true
 	}
-	if(s[0]==s[len(s)-1]){
-		return checkPalin(s[1:len(s)-1])
+	if s[0] == s[len(s)-1] {
+		return checkPalin(s[1 : len(s)-1])
 	}
 	return false
 }
-func TestCheckPalin(t *testing.T){
+func TestCheckPalin(t *testing.T) {
 	fmt.Println(checkPalin("racecar"))
 	fmt.Println(checkPalin("halkah"))
 }
-func decimaltoBinary(n int)string{
-	if(n==0){
+func decimaltoBinary(n int) string {
+	if n == 0 {
 		return ""
 	}
 	return decimaltoBinary(n/2) + strconv.Itoa(n%2)
 }
-func TestDecToBinary(t *testing.T){
+func TestDecToBinary(t *testing.T) {
 	fmt.Println(decimaltoBinary(7))
 }
