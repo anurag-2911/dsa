0e3806eaeed00fbc25dec6dc69391c93ddf24fa5
Anurag Kumar <anurag.hcu@gmail.com>
2023-12-01 11:33:48 +0530

    merge sort

--- recursion/rec_test.go
+++ recursion/rec_test.go
@@ -72,7 +72,7 @@ func TestSumOfNatural(t *testing.T) {
 }
 
 func xbinarysrch(n []int, search int) bool {
-	if(len(n)==0){
+	if len(n) == 0 {
 		return false
 	}
 	if len(n) == 1 && n[0] != search {
@@ -94,7 +94,6 @@ func xbinarysrch(n []int, search int) bool {
 		mid = (left + right) / 2
 		return xbinarysrch(n[mid:right], search)
 	}
-	
 
 }
 func TestXBinary(t *testing.T) {
@@ -105,21 +104,71 @@ func TestXBinary(t *testing.T) {
 	fmt.Println(xbinarysrch([]int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16}, 0))
 }
 
-func xfibonacci(n int)(int){
-	if(n==0 || n==1){
+func xfibonacci(n int) int {
+	if n == 0 || n == 1 {
 		return n
 	}
-	n1:=n-1
-	n2:=n-2
-	return xfibonacci(n1)+ xfibonacci(n2)
+	n1 := n - 1
+	n2 := n - 2
+	return xfibonacci(n1) + xfibonacci(n2)
 }
-func TestFibo(t *testing.T){
+func TestFibo(t *testing.T) {
 	//0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144
 	fmt.Println(xfibonacci(5))
 	fmt.Println(xfibonacci(6))
 	fmt.Println(xfibonacci(7))
 	fmt.Println(xfibonacci(8))
 
-
 }
 
+func TestXMergeSort(t *testing.T) {
+	arr := []int{38, 27, 43, 3, 9, 82, 10}
+	fmt.Println(arr)
+	fmt.Println(xmergeSort(arr))
+}
+
+func xmergeSort(arr []int) []int {
+	if len(arr) <= 1 {
+		return arr
+	}
+
+	//divide the array in two halves
+
+	mid := len(arr) / 2
+	left := xmergeSort(arr[:mid])
+	right := xmergeSort(arr[mid:])
+
+	//merge the two halves
+
+	return xmerge(left, right)
+}
+func xmerge(left []int, right []int) []int {
+
+	result := make([]int, 0, len(left)+len(right))
+
+	i, j := 0, 0
+
+	for i < len(left) && j < len(right) {
+		if left[i] < right[j] {
+			result = append(result, left[i])
+			i++
+		} else {
+			result = append(result, right[j])
+			j++
+		}
+	}
+	// for i<len(left){
+	// 	result = append(result, left[i])
+	// 	i++
+	// }
+	// for j<len(right){
+	// 	result = append(result, right[j])
+	// 	j++
+	// }
+
+	//append any remaining elements
+	result = append(result, left[i:]...)
+	result = append(result, right[j:]...)
+
+	return result
+}
