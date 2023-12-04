d21c64afe9ca8cb0b5f1f4f524fb8f420d36131d
Anurag Kumar <anurag.hcu@gmail.com>
2023-12-04 21:18:34 +0530

    Grid Traveler problem.

--- dynamicprogramming/dp01_test.go
+++ dynamicprogramming/dp01_test.go
@@ -11,7 +11,7 @@ func TestFunc(t *testing.T) {
 
 func TestXFibo(t *testing.T) {
 	//1,1,2,3,5,8,13,21,34....
-	fmt.Println(xfibmemo(6,make(map[int]int)))
+	fmt.Println(xfibmemo(6, make(map[int]int)))
 	fmt.Println(zoo(5))
 	fmt.Println(fibx(6))
 }
@@ -42,4 +42,38 @@ func xfibmemo(n int, result map[int]int) int {
 	res := xfibmemo(n-1, result) + xfibmemo(n-2, result)
 	result[n] = res
 	return res
+}
+
+func TestXGrdTraveller(t *testing.T) {
+	fmt.Println(xGridMemoizedTraveller(3,3,make(map[string]int)))
+	fmt.Println(xGridTraveller(3, 3))
+}
+
+func xGridTraveller(r, c int) int {
+	if r == 1 && c == 1 {
+		return 1
+	}
+	if r == 0 || c == 0 {
+		return 0
+	}
+	res := xGridTraveller(r-1, c) + xGridTraveller(r, c-1)
+	return res
+}
+func xGridMemoizedTraveller(r, c int, result map[string]int) int {
+	key := fmt.Sprintf("%d:%d", r, c)
+
+	if val, exists := result[key]; exists {
+		return val
+	}
+	if r == 1 && c == 1 {
+		return 1
+	}
+	if r == 0 || c == 0 {
+		return 0
+	}
+
+	result[key] = xGridMemoizedTraveller(r-1, c, result) + xGridMemoizedTraveller(r, c-1, result)
+
+	return result[key]
+
 }
