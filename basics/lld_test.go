f7eae433ed0f17eaa063a4bb7138f75b24f9f6b9
Anurag Kumar <anurag.hcu@gmail.com>
2024-01-21 23:31:31 +0530

    short url service.

--- basics/lld_test.go
+++ basics/lld_test.go
@@ -1,12 +1,78 @@
 package basics
 
-import
-(
-	"testing"
+import (
 	"fmt"
+	"net/http"
+	"sync"
+	"testing"
+
+	"github.com/gin-gonic/gin"
 )
 
-func TestXX(t *testing.T){
+func TestXX(t *testing.T) {
 	fmt.Println()
 }
+//short url basic implementation
 
+type XShortURlSvc struct {
+	mu      sync.Mutex
+	urlMap  map[string]string
+	baseurl string
+}
+
+func NewShortURlSvc(baseurl string) *XShortURlSvc {
+	return &XShortURlSvc{
+		urlMap:  make(map[string]string),
+		baseurl: baseurl,
+	}
+}
+func (shorturlsvc *XShortURlSvc) Shorten(originalurl string) string {
+	shorturlsvc.mu.Lock()
+	defer shorturlsvc.mu.Unlock()
+	short := fmt.Sprintf("%s/%d", shorturlsvc.baseurl, len(shorturlsvc.urlMap)+1)
+	shorturlsvc.urlMap[short] = originalurl
+	return short
+}
+func(shorturlsvc *XShortURlSvc)Resolve(shorturl string)(string,bool){
+	shorturlsvc.mu.Lock()
+	defer shorturlsvc.mu.Unlock()
+	if origurl,found:=shorturlsvc.urlMap[shorturl];found{
+		return origurl,found
+	}
+	return "",false
+}
+func TestShortURLSvc(t *testing.T){
+	router:=gin.Default()
+	shorturlsvc:=NewShortURlSvc("http://localhost:9090")
+	router.GET("/:shorturl",func(c *gin.Context){
+		param:=c.Param("shorturl")
+		shorturl:=shorturlsvc.baseurl + "/"+param
+		if origurl,found:=shorturlsvc.Resolve(shorturl);found{
+			c.Redirect(http.StatusMovedPermanently,origurl)
+		}else{
+			c.String(http.StatusNotFound,"url not found")
+		}
+
+	})
+	router.POST("/shorten",func (c *gin.Context){
+
+		var shortreq struct{
+			OriginalURL string `json:"originalurl"`
+		}
+		if c.BindJSON(&shortreq)==nil{
+			shorturl:=shorturlsvc.Shorten(shortreq.OriginalURL)
+			c.JSON(http.StatusOK,gin.H{"shorturl":shorturl})
+		}else{
+			c.JSON(http.StatusBadRequest,gin.H{"error ":"invalid request"})
+		}
+	})
+	router.Run(":9090")
+}
+
+//end of shorturl basic implementation
+
+// LRU cache
+
+
+
+// end of LRU cache
--- basics/progs_test.go
+++ basics/progs_test.go
@@ -1,11 +1,87 @@
 package basics
 
-import
-(
-	"testing"
+import (
 	"fmt"
+	"testing"
 )
 
 func TestXxx(t *testing.T) {
 	fmt.Println()
 }
+
+/*
+Fibonacci series: 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144
+*/
+
+func TestFib(t *testing.T) {
+	// memoized fibo recursive algo
+	fmt.Println(fibmemo(5, make(map[int]int)))
+	fmt.Println(fibmemo(6, make(map[int]int)))
+	fmt.Println(fibmemo(50, make(map[int]int)))
+
+	// simple fibo recursive algo
+	fmt.Println(fibo(5))
+	fmt.Println(fibo(6))
+	fmt.Println(fibo(50))
+
+}
+
+func fibo(n int) int {
+	if n <= 1 {
+		return n
+	}
+	return fibo(n-1) + fibo(n-2)
+}
+func fibmemo(n int, memo map[int]int) int {
+	if val, found := memo[n]; found {
+		return val
+	}
+	if n <= 1 {
+		return n
+	}
+	memo[n] = fibmemo(n-1, memo) + fibmemo(n-2, memo)
+	return memo[n]
+}
+
+/*
+Grid traveler problem
+*/
+
+func TestGridTraveler(t *testing.T) {
+	fmt.Println(gridTravelermemo(2, 3,make(map[string]int)))
+	fmt.Println(gridTravelermemo(3, 2,make(map[string]int)))
+	fmt.Println(gridTravelermemo(3, 3,make(map[string]int)))
+	fmt.Println(gridTravelermemo(30, 30,make(map[string]int)))
+
+	fmt.Println(gridTraveler(2, 3))
+	fmt.Println(gridTraveler(3, 2))
+	fmt.Println(gridTraveler(3, 3))
+	fmt.Println(gridTraveler(30, 30))
+}
+func gridTraveler(x, y int) int {
+	if x == 0 || y == 0 {
+		return 0
+	}
+	if x == 1 && y == 1 {
+		return 1
+	}
+	return gridTraveler(x-1, y) + gridTraveler(x, y-1)
+}
+func gridTravelermemo(x, y int, memo map[string]int) int {
+	key := fmt.Sprintf("%d:%d", x, y)
+	if val, found := memo[key]; found {
+		return val
+	}
+	if x == 0 || y == 0 {
+		memo[key] = 0
+		return memo[key]
+	}
+	if x == 1 && y == 1 {
+		memo[key] = 1
+		return memo[key]
+	}
+
+	memo[key] = gridTravelermemo(x-1, y, memo) + gridTravelermemo(x, y-1, memo)
+	return memo[key]
+}
+
