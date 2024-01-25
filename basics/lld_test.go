d576e2b6f10a9ce47d81bbf69ba4ffdee82f72c7
Anurag Kumar <anurag.hcu@gmail.com>
2024-01-25 21:39:12 +0530

    stack and queue as an array.

--- basics/lld_test.go
+++ basics/lld_test.go
@@ -5,6 +5,7 @@ import (
 	"net/http"
 	"sync"
 	"testing"
+	"time"
 
 	"github.com/gin-gonic/gin"
 )
@@ -12,6 +13,7 @@ import (
 func TestXX(t *testing.T) {
 	fmt.Println()
 }
+
 //short url basic implementation
 
 type XShortURlSvc struct {
@@ -33,37 +35,37 @@ func (shorturlsvc *XShortURlSvc) Shorten(originalurl string) string {
 	shorturlsvc.urlMap[short] = originalurl
 	return short
 }
-func(shorturlsvc *XShortURlSvc)Resolve(shorturl string)(string,bool){
+func (shorturlsvc *XShortURlSvc) Resolve(shorturl string) (string, bool) {
 	shorturlsvc.mu.Lock()
 	defer shorturlsvc.mu.Unlock()
-	if origurl,found:=shorturlsvc.urlMap[shorturl];found{
-		return origurl,found
+	if origurl, found := shorturlsvc.urlMap[shorturl]; found {
+		return origurl, found
 	}
-	return "",false
+	return "", false
 }
-func TestShortURLSvc(t *testing.T){
-	router:=gin.Default()
-	shorturlsvc:=NewShortURlSvc("http://localhost:9090")
-	router.GET("/:shorturl",func(c *gin.Context){
-		param:=c.Param("shorturl")
-		shorturl:=shorturlsvc.baseurl + "/"+param
-		if origurl,found:=shorturlsvc.Resolve(shorturl);found{
-			c.Redirect(http.StatusMovedPermanently,origurl)
-		}else{
-			c.String(http.StatusNotFound,"url not found")
+func TestShortURLSvc(t *testing.T) {
+	router := gin.Default()
+	shorturlsvc := NewShortURlSvc("http://localhost:9090")
+	router.GET("/:shorturl", func(c *gin.Context) {
+		param := c.Param("shorturl")
+		shorturl := shorturlsvc.baseurl + "/" + param
+		if origurl, found := shorturlsvc.Resolve(shorturl); found {
+			c.Redirect(http.StatusMovedPermanently, origurl)
+		} else {
+			c.String(http.StatusNotFound, "url not found")
 		}
 
 	})
-	router.POST("/shorten",func (c *gin.Context){
+	router.POST("/shorten", func(c *gin.Context) {
 
-		var shortreq struct{
+		var shortreq struct {
 			OriginalURL string `json:"originalurl"`
 		}
-		if c.BindJSON(&shortreq)==nil{
-			shorturl:=shorturlsvc.Shorten(shortreq.OriginalURL)
-			c.JSON(http.StatusOK,gin.H{"shorturl":shorturl})
-		}else{
-			c.JSON(http.StatusBadRequest,gin.H{"error ":"invalid request"})
+		if c.BindJSON(&shortreq) == nil {
+			shorturl := shorturlsvc.Shorten(shortreq.OriginalURL)
+			c.JSON(http.StatusOK, gin.H{"shorturl": shorturl})
+		} else {
+			c.JSON(http.StatusBadRequest, gin.H{"error ": "invalid request"})
 		}
 	})
 	router.Run(":9090")
@@ -73,6 +75,117 @@ func TestShortURLSvc(t *testing.T){
 
 // LRU cache
 
+/*
+double linked list, elements are added to the front, when read they are deleted from the current position
+and added to the front
+when adding if capacity is reached last element is removed
+*/
+type XNode struct {
+	next, prev *XNode
+	key, value int
+}
+type XLRUCache struct {
+	head, tail *XNode
+	lrucache   map[int]*XNode
+	capacity   int
+}
 
+func NewLRUCache(cap int) *XLRUCache {
+	return &XLRUCache{
+		lrucache: make(map[int]*XNode),
+		capacity: cap,
+	}
+}
+func (lru *XLRUCache) Add(newNode *XNode) {
+	if lru.head == nil {
+		lru.head = newNode
+		lru.tail = newNode
+		return
+	}
+	current := lru.head
+	newNode.next = current
+
+}
+
+func TestLRUCache(t *testing.T) {
+	lrucache := NewLRUCache(10)
+	node1 := &XNode{
+		key:   1,
+		value: 101,
+	}
+	node2 := &XNode{
+		key:   2,
+		value: 102,
+	}
+	node3 := &XNode{
+		key:   3,
+		value: 103,
+	}
+	lrucache.Add(node1)
+	lrucache.Add(node2)
+	lrucache.Add(node3)
+}
 
 // end of LRU cache
+
+/* Rate Limiter
+
+Token bucket algorithm.
+This algorithm allows for a certain number of requests (tokens) to be processed in a given time frame,
+replenishing the tokens at a steady rate.
+
+end of Rate Limiter*/
+
+type RateLimiter struct {
+	interval time.Duration
+	maxToken int
+	tokens   int
+	lock     sync.Mutex
+	ticker   time.Ticker
+}
+
+func NewRateLimiter(maxtokens int, interval time.Duration) *RateLimiter {
+	rl := &RateLimiter{
+		interval: interval,
+		maxToken: maxtokens,
+		tokens:   maxtokens,
+		ticker:   *time.NewTicker(interval),
+	}
+	go rl.start()
+	return rl
+}
+func (rl *RateLimiter) start() {
+	for range rl.ticker.C {
+		rl.lock.Lock()
+		defer rl.lock.Unlock()
+		if rl.tokens < rl.maxToken {
+			rl.tokens++
+		}
+	}
+}
+func (rl *RateLimiter) allow() bool {
+	rl.lock.Lock()
+	defer rl.lock.Unlock()
+
+	if rl.tokens > 0 {
+		rl.tokens--
+		return true
+	}
+	return false
+}
+func TestRateLimiter(t *testing.T) {
+	// Create a rate limiter that allows 5 requests per second
+	limiter := NewRateLimiter(5, time.Second)
+
+	// Example usage
+	for i := 0; i < 10; i++ {
+		if limiter.allow() {
+			fmt.Println("Request", i, "allowed")
+		} else {
+			fmt.Println("Request", i, "denied")
+		}
+		time.Sleep(100 * time.Millisecond)
+	}
+}
+
+//end of rate limiter
--- basics/progs_test.go
+++ basics/progs_test.go
@@ -48,10 +48,10 @@ Grid traveler problem
 */
 
 func TestGridTraveler(t *testing.T) {
-	fmt.Println(gridTravelermemo(2, 3,make(map[string]int)))
-	fmt.Println(gridTravelermemo(3, 2,make(map[string]int)))
-	fmt.Println(gridTravelermemo(3, 3,make(map[string]int)))
-	fmt.Println(gridTravelermemo(30, 30,make(map[string]int)))
+	fmt.Println(gridTravelermemo(2, 3, make(map[string]int)))
+	fmt.Println(gridTravelermemo(3, 2, make(map[string]int)))
+	fmt.Println(gridTravelermemo(3, 3, make(map[string]int)))
+	fmt.Println(gridTravelermemo(30, 30, make(map[string]int)))
 
 	fmt.Println(gridTraveler(2, 3))
 	fmt.Println(gridTraveler(3, 2))
@@ -85,3 +85,83 @@ func gridTravelermemo(x, y int, memo map[string]int) int {
 	return memo[key]
 }
 
+/*
+Stack as an array
+*/
+type XStack struct {
+	items []int
+}
+
+func (st *XStack) Push(item int) {
+	st.items = append(st.items, item)
+}
+func (stack *XStack) Pop() (int, error) {
+	if stack.IsEmpty() {
+		return 0, fmt.Errorf("empty stack")
+	}
+	length := len(stack.items)
+
+	item, items := stack.items[length-1], stack.items[0:length-1]
+
+	stack.items = items
+
+	return item, nil
+}
+func (stack *XStack) Peek() (int, error) {
+	if stack.IsEmpty() {
+		return 0, fmt.Errorf("empty stack")
+	}
+	length := len(stack.items)
+	return stack.items[length-1], nil
+}
+func (stack *XStack) IsEmpty() bool {
+	return len(stack.items) == 0
+}
+func TestStackOps(t *testing.T) {
+	stack := &XStack{
+		items: make([]int, 0),
+	}
+	stack.Push(101)
+	stack.Push(102)
+	stack.Push(103)
+	stack.Push(104)
+	fmt.Println(stack.Pop())
+	fmt.Println(stack.Peek())
+	fmt.Println(stack.IsEmpty())
+}
+
+/*
+Queue as an array
+*/
+type XQueue struct {
+	items []int
+}
+
+func (q *XQueue) Enqueue(item int) {
+	q.items = append(q.items, item)
+}
+func (q *XQueue) Dequeue() (int, error) {
+	if len(q.items) == 0 {
+		return 0, fmt.Errorf("empty queue")
+	}
+	item, items := q.items[0], q.items[1:]
+	q.items = items
+	return item, nil
+}
+func(q *XQueue)Peek()(int,error){
+	if len(q.items)==0{
+		return 0, fmt.Errorf("empty queue")
+	}
+	return q.items[0],nil
+}
+func TestQ(t *testing.T) {
+	queue := &XQueue{
+		items: make([]int, 0),
+	}
+	queue.Enqueue(101)
+	queue.Enqueue(102)
+	queue.Enqueue(103)
+	queue.Enqueue(104)
+	fmt.Println(queue.Dequeue())
+	fmt.Println(queue.Peek())
+}
