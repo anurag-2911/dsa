fc066d671d314d371f6707c18a530c788f3f1019
Anurag Kumar <anurag.hcu@gmail.com>
2024-02-06 08:15:24 +0530

    channels probs

--- basics/gobasics_test.go
+++ basics/gobasics_test.go
@@ -73,12 +73,12 @@ func TestGoSelect(t *testing.T) {
 	go func() {
 		time.Sleep(time.Second)
 		ch1 <- "Hallo Welt"
-		
+
 	}()
 	go func() {
-		time.Sleep(2*time.Second)
+		time.Sleep(2 * time.Second)
 		ch2 <- "Hello World"
-		
+
 	}()
 
 	select {
@@ -98,65 +98,85 @@ func TestGoSelect(t *testing.T) {
 /*
 Simple Goroutine Task:
 
-Write a program that starts 10 goroutines. 
-Each goroutine should generate a random number and print it to the console. 
+Write a program that starts 10 goroutines.
+Each goroutine should generate a random number and print it to the console.
 Use channels to synchronize the completion of all goroutines.
 */
 
-func TestSimpleGoRoutineTask(t *testing.T){
-	defer func(){
-		if r:=recover();r!=nil{
-			fmt.Println("recovered ",r)
+func TestSimpleGoRoutineTask(t *testing.T) {
+	defer func() {
+		if r := recover(); r != nil {
+			fmt.Println("recovered ", r)
 		}
 	}()
 	var wg sync.WaitGroup
-	
-	for i:=0;i<10;i++{
+
+	for i := 0; i < 10; i++ {
 		wg.Add(1)
 		go genRandomNumber(&wg)
 	}
 	wg.Wait()
 	fmt.Println("all tasks done")
 }
-func genRandomNumber(wg *sync.WaitGroup){
+func genRandomNumber(wg *sync.WaitGroup) {
 	defer wg.Done()
-	randomNum:=rand.Intn(100)
+	randomNum := rand.Intn(100)
 	fmt.Println(randomNum)
 }
-func TestSimpleGoRoutineTaskUsingChannels(t *testing.T){
-	defer func ()  {
-		if r:=recover();r!=nil{
-			fmt.Println("recovered ",r)
+func TestSimpleGoRoutineTaskUsingChannels(t *testing.T) {
+	defer func() {
+		if r := recover(); r != nil {
+			fmt.Println("recovered ", r)
 		}
 	}()
 	var wg sync.WaitGroup
-	ch:=make(chan int)
-	
-	for i:=0;i<10;i++{
+	ch := make(chan int)
+
+	for i := 0; i < 10; i++ {
 		wg.Add(1)
-		go genRanNum(&wg,ch)
+		go genRanNum(&wg, ch)
 	}
-	
-	go func(){
+
+	go func() {
 		wg.Wait()
 		close(ch)
 	}()
-	
-	for val:=range ch{
+
+	for val := range ch {
 		fmt.Print(val)
 		fmt.Print(" ")
 	}
 	fmt.Println("all tasks are done")
 }
-func genRanNum(wg *sync.WaitGroup, ch chan int){
+func genRanNum(wg *sync.WaitGroup, ch chan int) {
 	defer wg.Done()
-	ch<-rand.Intn(100)
+	ch <- rand.Intn(100)
 }
 
 /*
 Producer-Consumer Problem:
 
-Implement the classic producer-consumer scenario where you have two goroutines communicating through a channel. 
-One goroutine (the producer) generates data and sends it over a channel, 
+Implement the classic producer-consumer scenario where you have two goroutines communicating through a channel.
+One goroutine (the producer) generates data and sends it over a channel,
 while the other goroutine (the consumer) receives data from the channel and processes it.
 */
+
+func TestProdCons(t *testing.T) {
+	pc := ProdCons{}
+	ch:=make(chan int)
+	go pc.produce(ch)
+	go pc.consume(ch)
+	time.Sleep(time.Second)
+	fmt.Println(" all tasks done")
+}
+
+type ProdCons struct{}
+
+func (pc *ProdCons) produce(ch chan int) {
+	ch <- rand.Intn(100)
+	close(ch)
+}
+func (pc *ProdCons) consume(ch chan int) {
+	fmt.Print(<-ch)
+	fmt.Print()
+}
